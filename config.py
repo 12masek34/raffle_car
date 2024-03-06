763604839af07b0847b2661ac61f09bb0413fdc4
Dmitriy_Martys <12masek34@mail.ru>
2024-03-06 07:19:40 +0300

    добавил конфиг

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
+
 import logging
 import os
+from pathlib import (
+    Path,
+)
 
 from dotenv import (
     load_dotenv,
@@ -13,9 +17,13 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S"
 )
 log = logging.getLogger(__name__)
+
+pictures_dir = Path("static")
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE = os.getenv("POSTGRES_DB")
 USER = os.getenv("POSTGRES_USER")
 HOST = os.getenv("POSTGRES_HOST")
 PORT = os.getenv("POSTGRES_PORT")
 PASSWORD = os.getenv("POSTGRES_PASSWORD")
+GROUP_ID = os.getenv("GROUP_ID")
